b6f0e9f60d109511c486912fdc93d46be044497a
joaoalbertoxav <127453239+joaoalbertoxav@users.noreply.github.com>
2024-03-13 21:46:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//01 - Imprimir números de 1 a 10 
+
+//for (let index = 1; index <= 10; index++) {
+//    console.log(index) }
+
+
+//02 - Soma dos números de 1 a 10
+
+//let soma = 0
+//for (let index = 1; index <= 10; index++) {
+//    soma = soma + index
+
+//    console.log(soma) }
+
+
+//03 - Apresente a soma dos cem primeiros números inteiros
+
+//let soma = 0
+//for (let index = 1; index <= 100; index++) {
+//    soma = soma + index
+//    console.log(soma) }
+
+
+//04 - O quadrado dos números inteiros de 15 até 200
+
+//let soma = 2
+//for (let index = 15; index <= 200; index++) {
+  //  soma = soma*index
+  //  console.log(soma) } 
